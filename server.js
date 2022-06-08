49d3ff9476e9c4f2e905e7a1bbeacc09d51dd148
najwanan <theunnamedrd@gmail.com>
2022-06-07 22:21:01 -0700

    modified get statement to include input params

--- server.js
+++ server.js
@@ -1,57 +1,71 @@
-const express = require('express')
-const app = express()
-const PORT = 8000
+const express = require("express");
+const app = express();
+const PORT = 8000;
 
 const park = {
-  "Yosemite": {
+  'yosemite': {
     state: "California",
     peakSeason: "summer",
     lodging: "Rush Creek Lodge",
     bodyOfWater: "Yosemite falls",
-    wildlife: "true",
+    wildlife: false,
   },
 
-  "Joshua Tree": {
+  'joshua tree': {
     state: "California",
     peakSeason: "fall",
     lodging: "AirBnB",
     bodyOfWater: "none",
-    wildlife: "false",
+    wildlife: false,
   },
 
-  "Death Valley": {
+  "death valley": {
     state: "California",
     peakSeason: "spring",
     lodging: "Campsites",
     bodyOfWater: "none",
-    wildlife: "false",
+    wildlife: false,
   },
 
-  "Olympic": {
+  'olympic': {
     state: "Washington",
     peakSeason: "summer",
     lodging: "Olympic Lodge",
     bodyOfWater: "Lake Crescent",
-    wildlife: "true",
+    wildlife: true,
   },
 
-  "Mount Rainier": {
+  "mount rainier": {
     state: "Washington",
     peakSeason: "summer",
     lodging: "Paradise Village Hotel",
     bodyOfWater: "Summit Lake",
-    wildlife: "true",
+    wildlife: true,
+  },
+
+  'unknown': {
+    state: "unknown",
+    peakSeason: "unknown",
+    lodging: "unknown",
+    bodyOfWater: "unknown",
+    wildlife: false,
   },
 };
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
-})
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
 
-app.get('/api/:name', (request, response) => {
-    response.json(park)
-})
+app.get("/api/:name", (request, response) => {
+    const parkName = request.params.name.toLowerCase()
+    if (park[parkName]){
+        response.json(park[parkName])
+    } else {
+        response.json(park['unknown'])
+    }
+  response.json(park);
+});
 
-app.listen(process.env.PORT ||PORT, () => {
-    console.log(`The server is running on port 8000`)
-})
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`The server is running on port ${PORT}`);
+});
