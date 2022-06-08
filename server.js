d2e605d97be4060abb088858d4b2b94a339fad33
najwanan <theunnamedrd@gmail.com>
2022-06-07 21:57:58 -0700

    adding park objects

--- server.js
+++ server.js
@@ -2,10 +2,56 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const park = {
+  "Yosemite": {
+    state: "California",
+    peakSeason: "summer",
+    lodging: "Rush Creek Lodge",
+    bodyOfWater: "Yosemite falls",
+    wildlife: "true",
+  },
+
+  "Joshua Tree": {
+    state: "California",
+    peakSeason: "fall",
+    lodging: "AirBnB",
+    bodyOfWater: "none",
+    wildlife: "false",
+  },
+
+  "Death Valley": {
+    state: "California",
+    peakSeason: "spring",
+    lodging: "Campsites",
+    bodyOfWater: "none",
+    wildlife: "false",
+  },
+
+  "Olympic": {
+    state: "Washington",
+    peakSeason: "summer",
+    lodging: "Olympic Lodge",
+    bodyOfWater: "Lake Crescent",
+    wildlife: "true",
+  },
+
+  "Mount Rainier": {
+    state: "Washington",
+    peakSeason: "summer",
+    lodging: "Paradise Village Hotel",
+    bodyOfWater: "Summit Lake",
+    wildlife: "true",
+  },
+};
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.listen(PORT, () => {
+app.get('/api/:name', (request, response) => {
+    response.json(park)
+})
+
+app.listen(process.env.PORT ||PORT, () => {
     console.log(`The server is running on port 8000`)
 })
